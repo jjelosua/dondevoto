844afa1290077937fff22bfd59b103793736d9a6
Juan Elosua <juan.elosua@gmail.com>
2015-08-18 15:28:02 -0300

    Add fabric task to download crowdsourced results

--- backend/fabfile.py
+++ backend/fabfile.py
@@ -1,9 +1,32 @@
 # coding: utf-8
 from __future__ import with_statement
 from fabric.api import *
+from contextlib import contextmanager as _contextmanager
 import os
 # LOCAL PATHS
 cwd = os.path.dirname(__file__)
+SERVER_ROOT = os.environ.get('SERVER_ROOT', 'path_to_server_root')
+
+# Server config
+SERVER = os.environ.get('SERVER', 'hostname_or_ip')
+DB = os.environ.get('DB', 'dbname')
+env.user = os.environ.get('SSH_USER', 'username')
+
+# Role definitions
+env.roledefs = {
+    'server': [SERVER]
+}
+
+
+@_contextmanager
+def rvirtualenv():
+    SERVER_VIRTUALENV_PATH = os.path.join(
+        SERVER_ROOT,
+        'arg/elecciones_arg_2015_paso/backend/.venv'
+    )
+    with cd(SERVER_VIRTUALENV_PATH):
+        with prefix('source bin/activate'):
+            yield
 
 
 @task(alias='dedupe')
@@ -28,6 +51,26 @@ def regenerate_data():
         local('scripts/csvlink/clean_csvlink_weighted.sh')
         local('scripts/DB/load_weighted_matches.sh')
         local('python scripts/join_establecimientos_escuelas.py')
+
+
+@task(alias='getgeodata')
+@roles('server')
+def get_geo_data():
+    local_path = os.path.join(
+        cwd,
+        'data/geo'
+    )
+    db_backup_path = os.path.join(
+        SERVER_ROOT,
+        'db_backup'
+    )
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+    with lcd(local_path):
+        with cd(db_backup_path):
+            run('pg_dump -O %s | bzip2 > %s.sql.bz2' %
+                (DB, DB))
+            get('%s.sql.bz2' % (DB), '.')
 
 
 # DEFAULT TASK
